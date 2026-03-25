28825b122ac0a57006b5f51684d4edceaf871667
Durgaprasad <durgaprasadgirepunje@gmail.com>
2026-03-26 00:52:27 +0530

    Static KeyWord explained in better way

--- CoreJava/StaticKeyWord.java
+++ CoreJava/StaticKeyWord.java
@@ -8,21 +8,31 @@ public class StaticKeyWord {
 
     // The static variable also should not be initialized at constructor. hai Ki nhi like kyu hi apne ko hrr baar usse initialize krna hai jbb vo static variable sbb ke liye same hi hone wala hai
 
-    // So Rather than this what we will be doing is we will be using the static block which will be running only once when the class is loaded and at that time we will be initializing the static variable.
+    // So Rather than this what we will be doing is we will be using the STATIC block which will be running only once when the class is loaded and at that time we will be initializing the static variable.
 
+    //Static block Always runs first because the class loads the first then objects are instantiated.
 
     // Below is the example,
 
     // This is how we can say that static makes variables or methods or classes , object independent.
-    public static void main(String[] args) {
-        Human h1 = new Human();
-        h1.age = 10;
-        h1.name = "DDDD";
-        Human h2 = new Human();
-        h2.age = 23;
-        h2.name = "KKKK";
 
-        h2.show();
+
+    // --------- BUT ONE POINT IS TO NOT HERE IS ----
+    // If we don't create an object of a class the static block will also not run because it will not load the class as well
+
+    // To make the class to be loaded without the Object creation, we just use Class Class. Yeah there is a class named as class in java.
+     // In the class class we have a method as forName taking the className there. The One precaution to be taken here is it throws error so we have to handle it.
+
+    public static void main(String[] args) throws ClassNotFoundException {
+//        Human h1 = new Human();
+//        h1.age = 10;
+//        h1.name = "DDDD";
+//        Human h2 = new Human();
+//        h2.age = 23;
+//        h2.name = "KKKK";
+//
+//        h2.show();
+        Class.forName("Human");
 
         
 
@@ -39,6 +49,7 @@ class Human{
 
     static{
         roll = 57;
+        System.out.println("Human Static Block");
     }
     Human(){
     }
